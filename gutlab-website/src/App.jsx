56c4c2444d32c966fd9fd9fca2a0efe1d44b7bf9
gutlabbyxaels <goodfatbakery18@gmail.com>
2026-04-01 14:22:16 +0800

    Update App.jsx

--- gutlab-website/src/App.jsx
+++ gutlab-website/src/App.jsx
@@ -1,28 +1,9 @@
 export default function App() {
   return (
-    <div
-      style={{
-        minHeight: "100vh",
-        background: "#fff7eb",
-        color: "#21451f",
-        fontFamily: "Arial, sans-serif",
-        padding: "40px",
-      }}
-    >
-      <h1>Gut Lab by Xaels</h1>
-      <p>If you can see this, the React app is working.</p>
-
-      <img
-        src="/logo.png"
-        alt="Logo"
-        style={{ width: "120px", display: "block", marginTop: "20px" }}
-      />
-
-      <img
-        src="/healthy_spread.jpeg"
-        alt="Healthy Spread"
-        style={{ width: "240px", display: "block", marginTop: "20px", borderRadius: "16px" }}
-      />
+    <div style={{ padding: 40, fontFamily: "Arial, sans-serif" }}>
+      <h1>TEST PAGE WORKING</h1>
+      <p>If you can see this, you edited the correct file.</p>
+      <img src="/logo.png" alt="Logo" style={{ width: 120 }} />
     </div>
   );
 }
