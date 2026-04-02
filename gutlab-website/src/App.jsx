90c7db570f1b973301be50e2e86fe6abda612055
gutlabbyxaels <goodfatbakery18@gmail.com>
2026-04-02 20:00:55 +0800

    Update App.jsx

--- gutlab-website/src/App.jsx
+++ gutlab-website/src/App.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Gut Lab Website Working ✅</h1>
+      <img src="/logo.png" style={{ width: 120 }} />
+    </div>
+  );
+}
